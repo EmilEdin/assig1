0b06cff4328611f4d65562b41044b36368c8c60b
Emil Edin <emed6527@it.uu.se>
2023-09-11 14:21:55 +0200

    Added basic info

---
+++ hash_table.c
@@ -0,0 +1,11 @@
+#include "hash_table.h"
+
+ioopm_hash_table_t *ioopm_hash_table_create(void) {
+ // TODO: Stub
+  return NULL;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+  // TODO: Stub
+ return;
+}
