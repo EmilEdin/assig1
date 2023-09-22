2d446de54cbe8d50b1b5c9623d2402d125a4dc69
Emil Edin <emed6527@it.uu.se>
2023-09-22 12:17:11 +0200

    Did some linked list functions

--- linked_list.c
+++ linked_list.c
@@ -4,3 +4,25 @@
 #include <assert.h>
 #include <stdbool.h>
 #include "string.h"
+
+struct list {
+    int value;
+    ioopm_list_t *next;
+};
+
+ioopm_list_t *ioopm_linked_list_create(void) {
+    ioopm_list_t *new_list = calloc(1, sizeof(ioopm_list_t));
+    return new_list;
+}
+
+void ioopm_linked_list_destroy(ioopm_list_t *list) {
+    ioopm_list_t *next = list->next;
+    free(list);
+    if (next != NULL) {
+        ioopm_linked_list_destroy(next);
+    }
+}
+
+void ioopm_linked_list_append(ioopm_list_t *list, int value) {
+    
+}
